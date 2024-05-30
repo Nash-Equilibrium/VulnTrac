e52464bf6171b2c8f8e9e8de2e0bf7e2e09174be
ray <439824791@qq.com>
2024-05-30 21:23:23 +0800

    新一版

--- vulnCore/util/ASTTextSplitter.py
+++ vulnCore/util/ASTTextSplitter.py
@@ -1,5 +1,4 @@
 from tree_sitter import Parser, Language
-import re
 
 
 class ASTTextSplitter:
@@ -27,70 +26,67 @@ class ASTTextSplitter:
         parser = Parser()
         LANGUAGE = Language("build/my-languages.so", language)
         parser.set_language(LANGUAGE)
-        tree = parser.parse(bytes(text, "utf-8"))
+        text_bytes = text.encode(encoding="utf-8")
+        tree = parser.parse(text_bytes)
 
-        nodes = list(tree.root_node.children)
+        nodes = (node for node in tree.root_node.children)
         chunks = []
-        current_chunk = []
+        current_chunk = bytearray()
         counter = 0
-        node_num = 0
         final_end_byte = 0
 
-        # 获取节点的起始和结束字节
         def get_node_range(node):
+            """
+            获取节点的起始和结束字节位置
+            """
             start_byte = node.start_byte
             end_byte = node.end_byte
             return start_byte, end_byte
 
-        # 根据counter分割代码块
         def split_node(counter):
+            """
+            根据counter切分节点
+            """
             if counter >= self.node_number:
                 if self.overlap == 0:
-                    chunks.append("".join(current_chunk))
+                    chunks.append(bytes(current_chunk))
                     current_chunk.clear()
                 else:
                     overlap_chunk = current_chunk[-self.overlap :]
-                    chunks.append("".join(current_chunk))
-                    current_chunk = overlap_chunk
+                    chunks.append(bytes(current_chunk))
+                    current_chunk = bytearray(overlap_chunk)
                 return 0
             return counter
 
-        # 处理文件开头的代码
         if nodes:
-            start_byte, _ = get_node_range(nodes[0])
-            current_chunk.append(text[:start_byte])
+            start_byte, _ = get_node_range(next(nodes))
+            current_chunk.extend(text_bytes[:start_byte])
 
         for node in nodes:
             if node.type in ["function_definition", "class_definition"]:
                 start_byte, end_byte = get_node_range(node)
-                parent_chunk = text[start_byte:end_byte]
+                parent_chunk = text_bytes[start_byte:end_byte].strip()
 
-                # 处理嵌套定义
-                nested_nodes = list(node.children)
+                nested_nodes = (nested_node for nested_node in node.children)
                 for nested_node in nested_nodes:
                     if nested_node.type in ["function_definition", "class_definition"]:
                         nested_start, nested_end = get_node_range(nested_node)
-                        nested_chunk = text[nested_start:nested_end]
-                        current_chunk.append(nested_chunk)
+                        nested_chunk = text_bytes[nested_start:nested_end]
+                        current_chunk.extend(nested_chunk)
                         counter += 1
                         counter = split_node(counter)
 
-                        # 从父节点中删除嵌套节点
-                        parent_chunk = parent_chunk.replace(nested_chunk, "")
+                        parent_chunk = parent_chunk.replace(nested_chunk, b"")
 
                 if parent_chunk:
-                    current_chunk.append(parent_chunk)
+                    current_chunk.extend(parent_chunk)
                     counter += 1
                     counter = split_node(counter)
 
-                if node_num == len(nodes) - 1:
+                if node is list(tree.root_node.children)[-1]:
                     final_end_byte = end_byte
-                node_num += 1
 
-        # 处理文件结尾的代码
-        chunks.append("".join(current_chunk) + text[final_end_byte:])
+        chunks.append(bytes(current_chunk) + text_bytes[final_end_byte:])
 
-        chunks = [re.sub(r"^\n+", "", chunk) for chunk in chunks]
-        chunks = [re.sub(r"\n+$", "", chunk) for chunk in chunks]
-
+        chunks = [chunk.decode() for chunk in chunks]
         return chunks
