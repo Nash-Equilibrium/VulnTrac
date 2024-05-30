0071a4b7c605a115c510b7ed2dc5b10db66f2d37
ray <439824791@qq.com>
2024-05-30 20:45:45 +0800

    测试

--- vulnCore/util/test.py
+++ vulnCore/util/test.py
@@ -4,26 +4,39 @@ from ASTTextSplitter import ASTTextSplitter
 myast = ASTTextSplitter(node_number=2, overlap=1, chunk_size=400)
 
 python_code = """
-def foo():
-    pass
+import smtplib
+from email.mime.text import MIMEText
+from email.utils import formataddr
 
-class Bar:
-    def baz():
-        pass
 
-    def qux():
-        pass
+def mailsend(my_sender, my_pass, my_user, text):
+    ret = True
+    try:
+        msg = MIMEText("验证码为：" + text, "plain", "utf-8")
+        msg["From"] = formataddr(
+            ["From VualTrac", my_sender]
+        )  # 括号里的对应发件人邮箱昵称、发件人邮箱账号
+        msg["To"] = formataddr(
+            ["For you", my_user]
+        )  # 括号里的对应收件人邮箱昵称、收件人邮箱账号
+        msg["Subject"] = "验证码"
 
-def corge():
-    pass
+        server = smtplib.SMTP_SSL("smtp.qq.com", 465)
+        server.login(my_sender, my_pass)
+        server.sendmail(
+            my_sender,
+            [
+                my_user,
+            ],
+            msg.as_string(),
+        )  # 括号中对应的是发件人邮箱账号、收件人邮箱账号、发送邮件
+        server.quit()
 
-class Grault:
-    def garply():
-        pass
+    except Exception:
+        ret = False
+    return ret
 
-    def waldo():
-        def nested_func():
-            pass
+
 """
 result = myast.create_documents(python_code, "python")
 print(result)
